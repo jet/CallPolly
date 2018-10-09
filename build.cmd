c51a43c8aeee87aafb282a3c5b50aa03b7c5aade
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2018-10-09 18:16:30 +0100

    Add build.cmd

---
+++ build.cmd
@@ -0,0 +1,5 @@
+dotnet pack src/CallPolly --configuration Release -o "%CD%\bin" --version-suffix CI%1
+if ERRORLEVEL 1 (echo Error building CallPolly; exit /b 1)
+
+dotnet test tests/CallPolly.Tests --configuration Release	
+if ERRORLEVEL 1 (echo Error testing CallPolly; exit /b 1)
