31dc39d60e34ddecfffd6bc30c2a74f5ec9e069e
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2018-10-11 18:44:43 +0100

    Pad build numbers and prefix PRs
    This will create stuff like:
    `MyLib.0.1.0-CI0000798.nupkg` (from master)
    `MyLib.0.1.0-pr17-0000798.nupkg` (from PRs)

--- build.cmd
+++ build.cmd
@@ -1,4 +1,10 @@
-dotnet pack src/CallPolly --configuration Release -o "%CD%\bin" --version-suffix CI%1
+SETLOCAL
+SET X=%1
+SET BLD=000000%X%
+SET BLD=%BLD:~-7%
+if [%2]==[] (SET V=CI%BLD%) else (SET V=pr%2-%BLD%)
+
+dotnet pack src/CallPolly --configuration Release -o "%CD%\bin" --version-suffix %V%
 if ERRORLEVEL 1 (echo Error building CallPolly; exit /b 1)
 
 dotnet test tests/CallPolly.Tests --configuration Release
@@ -6,3 +12,5 @@ if ERRORLEVEL 1 (echo Error testing CallPolly; exit /b 1)
 
 dotnet test tests/CallPolly.Acceptance --configuration Release
 if ERRORLEVEL 1 (echo Error acceptance testing CallPolly; exit /b 1)
+
+ENDLOCAL
