98fc824919f049da59c03c6a44853ddf8cf5af62
Ruben Bartelink <ruben@bartelink.com>
2018-10-18 03:37:56 +0100

    Move build logic to build.proj

--- build.cmd
+++ build.cmd
@@ -1,16 +1,2 @@
-SETLOCAL
-SET X=%1
-SET BLD=000000%X%
-SET BLD=%BLD:~-7%
-if [%2]==[] (SET V=CI%BLD%) else (SET V=pr%2-%BLD%)
-
-dotnet pack src/CallPolly --configuration Release -o "%CD%\bin" --version-suffix %V%
-if ERRORLEVEL 1 (echo Error building CallPolly; exit /b 1)
-
-dotnet test tests/CallPolly.Tests --configuration Release
-if ERRORLEVEL 1 (echo Error testing CallPolly; exit /b 1)
-
-dotnet test tests/CallPolly.Acceptance --configuration Release
-if ERRORLEVEL 1 (echo Error acceptance testing CallPolly; exit /b 1)
-
-ENDLOCAL
+dotnet pack build.proj /p:BN=%1 /p:PR=%2
+dotnet test build.proj -v n
